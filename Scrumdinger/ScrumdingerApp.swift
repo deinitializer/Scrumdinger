bde04dd41f28a10d16c1c7a436ccaf4bcc5382e2
Lorenzo Mazzarotto <deinitializer@icloud.com>
2023-03-03 19:53:30 -0300

    Using stacks to arrange views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lorenzo Mazzarotto on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
